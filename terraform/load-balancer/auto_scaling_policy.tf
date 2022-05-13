2d5c3de5519ef48b93505de3ba9c28653fa9877c
Pranesh25 <41353968+Pranesh25@users.noreply.github.com>
2022-05-13 09:07:41 +0530

    Delete terraform/load-balancer directory

--- terraform/load-balancer/auto_scaling_policy.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_autoscaling_policy" "scalingPolicy" {
-  name                   = "scaling-terraform-test"
-  scaling_adjustment     = 3
-  adjustment_type        = "ChangeInCapacity"
-  policy_type            = "SimpleScaling"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.asg.name
-}
--- terraform/load-balancer/modules/providers.tf
+++
@@ -1,6 +0,0 @@
-# provider "aws" {
-#     region =  "${var.region}"
-#     secret_key =  "${var.secret_key}"
-#     access_key =   "${var.access_key }"
-
-# }
